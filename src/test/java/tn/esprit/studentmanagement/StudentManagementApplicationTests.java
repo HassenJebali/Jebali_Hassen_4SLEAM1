b0414c9e81e4098da3c590ef1991a4a84ef43c3f
jebali.hassen <hdjebali92@gmail.com>
2025-12-02 22:32:24 +0100

    config test1.1

--- src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
+++ src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
@@ -2,12 +2,19 @@ package tn.esprit.studentmanagement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import tn.esprit.studentmanagement.services.StudentService;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@ActiveProfiles("test")
 class StudentManagementApplicationTests {
+
+    @MockBean
+    private StudentService studentService;  // Mock du service pour ne pas toucher à la DB
 
     @Test
     void contextLoads() {
     }
-
 }
+
